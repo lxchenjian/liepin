949bdc6b3717c310bc24fbb28da4f2dd3f5864f8
“lxchenjian” <982861129@qq.com>
2026-03-14 11:18:42 +0800

    微服务网关Gateway与端口规范 构建微服务网关Gateway与负载均衡loadbalancer

--- service-user-7001/src/main/java/com/liepin/controller/HelloController.java
+++ service-user-7001/src/main/java/com/liepin/controller/HelloController.java
@@ -7,6 +7,7 @@ import com.liepin.pojo.test.Stu;
 import com.liepin.service.StuService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,9 @@ public class HelloController {
 
     @Autowired
     private StuService stuService;
+
+    @Value("${server.port}")
+    private String port;
 
     @GetMapping("stu")
     public Object stu() {
@@ -37,6 +41,9 @@ public class HelloController {
         log.debug("debug：" + stu.toString());
         log.warn("warn：" + stu.toString());
         log.error("error：{}", stu.toString());
+
+        log.info("lb测试，当前端口号为：" + port);
+
 
         return "Hello UserService~~~";
     }
